0c1049fc021c238c47b5f6689da930bfe78c5fff
Djimon Nowak <djimon@mail.uni-paderborn.de>
2025-01-19 17:10:06 +0100

    add tests for position class

---
+++ games/hnefatafl_stuff/test_position.py
@@ -0,0 +1,85 @@
+import copy
+from games.hnefatafl_stuff.hnefatafl import Hnefatafl
+from games.hnefatafl_stuff.piece_type import PieceType
+from games.hnefatafl_stuff.position import Position
+from games.hnefatafl_stuff.types import default_board
+
+
+def test_up():
+    test = Position(y=1, x=1)
+    up = test.up()
+    assert up.y == 2 and up.x == 1
+
+
+def test_down():
+    test = Position(y=1, x=1)
+    down = test.down()
+    assert down.y == 0 and down.x == 1
+
+
+def test_right():
+    test = Position(y=1, x=1)
+    right = test.right()
+    assert right.y == 1 and right.x == 2
+
+
+def test_left():
+    test = Position(y=1, x=1)
+    left = test.left()
+    assert left.y == 1 and left.x == 0
+
+
+def test_get_square():
+    board = copy.deepcopy(default_board)
+    assert Hnefatafl.MIDDLE.get_square(board=board) == PieceType.KING
+
+
+def test_set_square():
+    board = copy.deepcopy(default_board)
+    Hnefatafl.MIDDLE.set_square(board=board, piece=PieceType.ATTACKER)
+    assert Hnefatafl.MIDDLE.get_square(board=board) == PieceType.ATTACKER
+
+
+def test_is_within_board():
+    # is in
+    assert Hnefatafl.MIDDLE.is_within_board()
+
+    # is over
+    assert not Position(y=7, x=1).is_within_board()
+    assert not Position(y=8, x=1).is_within_board()
+
+    # is under
+    assert not Position(y=-1, x=1).is_within_board()
+    assert not Position(y=-2, x=1).is_within_board()
+
+    # is too left
+    assert not Position(y=2, x=-1).is_within_board()
+    assert not Position(y=2, x=-2).is_within_board()
+
+    # is too right
+    assert not Position(y=2, x=7).is_within_board()
+    assert not Position(y=2, x=8).is_within_board()
+
+
+def test_is_open_to_piece():
+    # not in field
+    assert not Position(7, 7).is_open_to_piece(piece=PieceType.ATTACKER)
+
+    # corner
+    assert not Position(0, 0).is_open_to_piece(piece=PieceType.ATTACKER)
+    assert not Position(6, 0).is_open_to_piece(piece=PieceType.ATTACKER)
+    assert not Position(0, 6).is_open_to_piece(piece=PieceType.ATTACKER)
+    assert not Position(6, 6).is_open_to_piece(piece=PieceType.ATTACKER)
+    assert not Position(0, 0).is_open_to_piece(piece=PieceType.DEFENDER)
+    assert not Position(6, 0).is_open_to_piece(piece=PieceType.DEFENDER)
+    assert not Position(0, 6).is_open_to_piece(piece=PieceType.DEFENDER)
+    assert not Position(6, 6).is_open_to_piece(piece=PieceType.DEFENDER)
+    assert Position(0, 0).is_open_to_piece(piece=PieceType.KING)
+    assert Position(6, 0).is_open_to_piece(piece=PieceType.KING)
+    assert Position(0, 6).is_open_to_piece(piece=PieceType.KING)
+    assert Position(6, 6).is_open_to_piece(piece=PieceType.KING)
+
+    # throne
+    assert not Hnefatafl.MIDDLE.is_open_to_piece(piece=PieceType.ATTACKER)
+    assert not Hnefatafl.MIDDLE.is_open_to_piece(piece=PieceType.DEFENDER)
+    assert Hnefatafl.MIDDLE.is_open_to_piece(piece=PieceType.KING)
--- games/hnefatafl_stuff/types.py
+++ games/hnefatafl_stuff/types.py
@@ -6,3 +6,79 @@ from games.hnefatafl_stuff.position import Position
 
 Move = Tuple[Position, Position]
 Board = List[List[Optional[PieceType]]]
+
+empty_board: Board = [
+    [None] * 7,
+    [None] * 7,
+    [None] * 7,
+    [None] * 7,
+    [None] * 7,
+    [None] * 7,
+    [None] * 7,
+]
+
+default_board: Board = [
+    [
+        None,
+        None,
+        None,
+        PieceType.ATTACKER,
+        None,
+        None,
+        None,
+    ],
+    [
+        None,
+        None,
+        None,
+        PieceType.ATTACKER,
+        None,
+        None,
+        None,
+    ],
+    [
+        None,
+        None,
+        None,
+        PieceType.DEFENDER,
+        None,
+        None,
+        None,
+    ],
+    [
+        PieceType.ATTACKER,
+        PieceType.ATTACKER,
+        PieceType.DEFENDER,
+        PieceType.KING,
+        PieceType.DEFENDER,
+        PieceType.ATTACKER,
+        PieceType.ATTACKER,
+    ],
+    [
+        None,
+        None,
+        None,
+        PieceType.DEFENDER,
+        None,
+        None,
+        None,
+    ],
+    [
+        None,
+        None,
+        None,
+        PieceType.ATTACKER,
+        None,
+        None,
+        None,
+    ],
+    [
+        None,
+        None,
+        None,
+        PieceType.ATTACKER,
+        None,
+        None,
+        None,
+    ],
+]
